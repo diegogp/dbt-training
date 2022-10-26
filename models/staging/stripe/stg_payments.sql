4c63b535f3736b2fb91db943ef4e2acef7b57053
93diegopereira@gmail.com <93diegopereira@gmail.com>
2022-10-26 20:44:05 +0000

    adding sources

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,16 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+with payments as (
 
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
 
-from dbt-tutorial.stripe.payment 
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
+
+    from {{source('stripe', 'payment')}}
+
+)
+select * from payments
